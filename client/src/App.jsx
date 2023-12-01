2a38b93ce2557e894142f89829e7c403e6f9a04e
chart099 <122659980+chart099@users.noreply.github.com>
2023-12-01 13:33:45 -0600

    Creating a header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import { setContext } from '@apollo/client/link/context';
 import { Outlet } from 'react-router-dom';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
+import Header from './components/Header';
 
 
 // Construct our main GraphQL API endpoint
@@ -38,6 +39,7 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
+      <Header />
       <Nav />
         <div>
           <Outlet />
