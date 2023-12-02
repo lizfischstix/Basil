49e9cfb61b51934a231b2ba17579b36ff8f5ca3a
Liz FIschenich <Liz.Devlin85@gmail.com>
2023-12-01 18:21:27 -0600

    prep to pull

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,10 +1,27 @@
+// import * as React from 'react';
+// import Tabs from '@mui/material/Tabs';
+// import Tab from '@mui/material/Tab';
+// import Box from '@mui/material/Box';
+// import { Link } from 'react-router-dom';
 
-const Nav = () => {
-  return (
-    <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
-        <h1>This is Nav for budget tracker</h1>
-    </header>
-  );
-};
+// export default function AccessibleTabs1() {
+//   const [value, setValue] = React.useState(0);
+//   const handleChange = (event, newValue) => {
+//     setValue(newValue);
+//   };
 
-export default Nav;
+//   return (
+//     <Box sx={{ width: '100%' }}>
+//       <Tabs
+//         onChange={handleChange}
+//         value={value}
+//         aria-label="Tabs where selection follows focus"
+//         selectionFollowsFocus
+//       >
+//        <Link to="/login"><Tab label="Log In"/> </Link>
+//        <Link to="/signup"><Tab label="Sign Up"/> </Link>
+//         <Link to="/" <Tab label="Item Three" />
+//       </Tabs>
+//     </Box>
+//   );
+// }
