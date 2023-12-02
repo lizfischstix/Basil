c7ccaf8b0228f94ca5d73e34fbbf42928e9cdbef
Liz FIschenich <Liz.Devlin85@gmail.com>
2023-12-01 18:59:11 -0600

    added footer with continuoutly updating copyright, added code in app.jsx that sets min VH to 100 so footer always appears at bottom of screan.

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,11 +1,30 @@
 
-const Footer = () => {
-    return (
-      <>
-        <footer>Basil</footer>
-      </>
+import React, { useState, useEffect } from 'react';
 
-    );
-  };
-  
-  export default Footer;
+const Footer = () => {
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+
+  useEffect(() => {
+    const updateYear = () => {
+      setCurrentYear(new Date().getFullYear());
+    };
+
+    // Update the year when the component mounts
+    updateYear();
+
+    // Optionally, you can set up a timer or other triggers to update the year periodically
+    // For example, update every 1000ms (1 second)
+    const intervalId = setInterval(updateYear, 1000);
+
+    // Cleanup the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <footer>
+      <p className='text-center'>&copy; {currentYear} Basil🌿. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
