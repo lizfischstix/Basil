06faa353a55c4344c83a0301914fa7c7027f2f66
Liz FIschenich <Liz.Devlin85@gmail.com>
2023-12-02 15:18:40 -0600

    prep to pull

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,16 +1,13 @@
 import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
-import LoginRoundedIcon from '@mui/icons-material/LoginRounded';
 import PhoneIcon from '@mui/icons-material/Phone';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import PersonPinIcon from '@mui/icons-material/PersonPin';
-import { Link } from 'react-router-dom';
-import LoginRounded from '@mui/icons-material/LoginRounded';
-import SubscriptionsRoundedIcon from '@mui/icons-material/SubscriptionsRounded';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import LoginRoundedIcon from '@mui/icons-material/LoginRounded';
 
-export default function Nav() {
+export default function Nav({ isAuthenticated }) {
   const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
@@ -18,15 +15,15 @@ export default function Nav() {
   };
 
   return (
-    <Tabs
-      value={value}
-      onChange={handleChange}
-      orientation="vertical" // Set the orientation to "vertical"
-      aria-label="icon tabs example"
-    >
-      <Link to="/"><Tab icon={<HomeRoundedIcon />} aria-label="Home" /></Link>
-      <Tab icon={<LoginRounded/>} aria-label="Log In" />
-      <Tab icon={<SubscriptionsRoundedIcon />} aria-label="Sign Up " />
-    </Tabs>
+    { isAuthenticated ? (
+    <Tabs value = { value } onChange = { handleChange } orientation = "vertical" aria- label= "icon tabs" >
+    <Tab icon={<HomeRoundedIcon />} aria-label="Home" />
+      
+    ) : (
+    <Tab icon={<LoginRoundedIcon />} aria-label="Log In" />
+  )
+}
+    </Tabs >
   );
 }
+
