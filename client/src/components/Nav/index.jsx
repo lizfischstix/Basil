aceae7433fdd55c766ba922e0b0a9f6adb9c72af
tanfei0704 <tanfei0704@gmail.com>
2023-12-03 10:55:58 -0500

    save

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -8,6 +8,7 @@ import AddBoxRoundedIcon from '@mui/icons-material/AddBoxRounded';
 import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
 import AssignmentRoundedIcon from '@mui/icons-material/AssignmentRounded';
 import AutoGraphRoundedIcon from '@mui/icons-material/AutoGraphRounded';
+import WysiwygRoundedIcon from '@mui/icons-material/WysiwygRounded';
 
 export default function Nav({ isAuthenticated }) {
   const [value, setValue] = React.useState(0);
@@ -28,7 +29,7 @@ export default function Nav({ isAuthenticated }) {
             <Tab icon={<LoginRoundedIcon />} aria-label="Log Out" />
           </Link>
           <Link to="/overview">
-            <Tab icon={<SettingsRoundedIcon />} aria-label="Overview" />
+            <Tab icon={<WysiwygRoundedIcon/>} aria-label="Overview" />
           </Link>
           <Tab icon={<AssignmentRoundedIcon />} aria-label="Transactions List" />
           <Tab icon={<AutoGraphRoundedIcon />} aria-label="Transactions Graphs" />
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,53 @@
 import React from 'react';
+import Auth from "../utils/auth";
+import { useQuery } from "@apollo/client";
 
-const Home = () => {
-  
+import { QUERY_ME } from "../utils/queries";
+
+const Home= () => {
+  // Check if the user is logged in
+  if (!Auth.loggedIn()) {
+    return <p>You need to be logged in to see this page.</p>;
+  }
+
+  // Fetch user data
+  const { loading, error, data } = useQuery(QUERY_ME);
+
+  // Handle loading state
+  if (loading) {
+    // You can use loading skeletons or placeholders here
+    return <div>Loading...</div>;
+  }
+
+  // Handle error state
+  if (error) {
+    console.error("Error fetching user data:", error);
+    return <p>An error occurred while fetching data.</p>;
+  }
+
+  // User data is available
+  const userInfo = data.me;
+
   return (
-      <>
-        <h3 className="container text-center">
-          Please login for more functions!
-        </h3>
-      </>
+    <>
+      <h2>Incomes</h2>
+      {userInfo.incomes.map((income) => (
+        <div key={income._id}>
+          <p>Description: {`${income.description}`}</p>
+          <p>Amount: ${`${income.amount}`}</p>
+        </div>
+      ))}
+      {userInfo.incomes.length === 0 && <p>No incomes found.</p>}
+
+      <h2>Expenses</h2>
+      {userInfo.expenses.map((expense) => (
+        <div key={expense._id}>
+          <p>Description: {`${expense.description}`}</p>
+          <p>Amount: ${`${expense.amount}`}</p>
+        </div>
+      ))}
+      {userInfo.expenses.length === 0 && <p>No expenses found.</p>}
+    </>
   );
 };
 
