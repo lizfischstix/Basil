c208d256322cdbbe60fed767e1e61a786bf7dd98
tanfei0704 <tanfei0704@gmail.com>
2023-12-04 09:15:46 -0500

    save

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -8,7 +8,6 @@ import AddBoxRoundedIcon from '@mui/icons-material/AddBoxRounded';
 import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
 import AssignmentRoundedIcon from '@mui/icons-material/AssignmentRounded';
 import AutoGraphRoundedIcon from '@mui/icons-material/AutoGraphRounded';
-import WysiwygRoundedIcon from '@mui/icons-material/WysiwygRounded';
 
 export default function Nav({ isAuthenticated }) {
   const [value, setValue] = React.useState(0);
@@ -29,7 +28,7 @@ export default function Nav({ isAuthenticated }) {
             <Tab icon={<LoginRoundedIcon />} aria-label="Log Out" />
           </Link>
           <Link to="/overview">
-            <Tab icon={<WysiwygRoundedIcon/>} aria-label="Overview" />
+            <Tab icon={<SettingsRoundedIcon />} aria-label="Overview" />
           </Link>
           <Tab icon={<AssignmentRoundedIcon />} aria-label="Transactions List" />
           <Tab icon={<AutoGraphRoundedIcon />} aria-label="Transactions Graphs" />
