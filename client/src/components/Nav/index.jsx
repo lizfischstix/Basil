a0403d5d6adc4de2050c32e385a01a38b28e7f28
ingridmidia <ingridmidia@gmail.com>
2023-12-06 13:13:50 -0600

    no line on navbar

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -31,39 +31,94 @@ export default function Nav({ isAuthenticated }) {
       onChange={handleChange}
       aria-label="icon tabs"
       orientation="vertical"
-      
+      indicatorColor= {{color: green[900]}}
     >
       <Link to="/">
-      <Tooltip title='Home' placement='bottom' arrow><Tab icon={<EnergySavingsLeafIcon sx={{ color: green[900], fontSize: 30 }}  />} aria-label="Home" /></Tooltip>
+        <Tooltip title="Home" placement="bottom" arrow>
+          <Tab
+            icon={
+              <EnergySavingsLeafIcon sx={{ color: green[900], fontSize: 30 }} />
+            }
+            aria-label="Home"
+          />
+        </Tooltip>
       </Link>
 
       {Auth.loggedIn() ? (
         <>
           <Link to="/overview">
-          <Tooltip title='Dashboard' placement='bottom' arrow> <Tab icon={<HomeRoundedIcon sx={{ color: green[900], fontSize: 30 }}/>} aria-label="Overview" /></Tooltip>
+            <Tooltip title="Dashboard" placement="bottom" arrow>
+              {" "}
+              <Tab
+                icon={
+                  <HomeRoundedIcon sx={{ color: green[900], fontSize: 30 }} />
+                }
+                aria-label="Overview"
+              />
+            </Tooltip>
           </Link>
-          
+
           <Link to="/transaction">
-          <Tooltip title='Transaction List' placement='bottom' arrow> 
-          <Tab icon={<AssignmentRoundedIcon sx={{ color: green[900], fontSize: 30 }}/>} aria-label="Transactions List"/></Tooltip>
+            <Tooltip title="Transaction List" placement="bottom" arrow>
+              <Tab
+                icon={
+                  <AssignmentRoundedIcon
+                    sx={{ color: green[900], fontSize: 30 }}
+                  />
+                }
+                aria-label="Transactions List"
+              />
+            </Tooltip>
           </Link>
-          
+
           <Link to="/graphpage">
-          <Tooltip title='Spending Graphs' placement='bottom' arrow> 
-          <Tab icon={<AutoGraphRoundedIcon sx={{ color: green[900], fontSize: 30 }} />} aria-label="Transactions Graphs"/></Tooltip>
+            <Tooltip title="Spending Graphs" placement="bottom" arrow>
+              <Tab
+                icon={
+                  <AutoGraphRoundedIcon
+                    sx={{ color: green[900], fontSize: 30 }}
+                  />
+                }
+                aria-label="Transactions Graphs"
+              />
+            </Tooltip>
           </Link>
 
           <Link to="/home">
-          <Tooltip title='Log Out' placement='bottom' arrow><Tab icon={<LoginRoundedIcon sx={{ color: green[900], fontSize: 30 }} />} aria-label="Home" onClick={logout}/></Tooltip>
+            <Tooltip title="Log Out" placement="bottom" arrow>
+              <Tab
+                icon={
+                  <LoginRoundedIcon sx={{ color: green[900], fontSize: 30 }} />
+                }
+                aria-label="Home"
+                onClick={logout}
+              />
+            </Tooltip>
           </Link>
         </>
       ) : (
         <>
           <Link to="/login">
-          <Tooltip title='Log In' placement='bottom' arrow>  <Tab icon={<LoginRoundedIcon sx={{ color: green[900], fontSize: 30 }} />} aria-label="Log In" /></Tooltip>
+            <Tooltip title="Log In" placement="bottom" arrow>
+              {" "}
+              <Tab
+                icon={
+                  <LoginRoundedIcon sx={{ color: green[900], fontSize: 30 }} />
+                }
+                aria-label="Log In"
+              />
+            </Tooltip>
           </Link>
           <Link to="/signup">
-          <Tooltip title='Sign Up' placement='bottom' arrow>  <Tab icon={<AddBoxRoundedIcon sx={{ color: green[900], fontSize: 30 }} />} aria-label="Sign Up" /></Tooltip>
+            <Tooltip title="Sign Up" placement="bottom" arrow>
+              {" "}
+              <Tab
+                icon={
+                  <AddBoxRoundedIcon sx={{ color: green[900], fontSize: 30 }} />
+                }
+                aria-label="Sign Up"
+              />
+            </Tooltip>
           </Link>
         </>
       )}
