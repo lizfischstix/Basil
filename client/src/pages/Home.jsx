7812de912ed910078c8c845d1c9298ee44c77e37
Liz FIschenich <Liz.Devlin85@gmail.com>
2023-12-06 22:54:22 -0600

    landing page is in good-enough-for-tonight shape.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,34 +4,39 @@ import { Box, Grid, Paper, Typography } from '@mui/material';
 import Graphic from '../assets/Images/basil-laptop.png'
 const Home = () => {
   return (
-    <Grid container spacing={{ xs: 4, md: 3 }} justifyContent="center">
-      {/* Image and Text Section */}
-      <Grid item xs={6} md={10}>
-        <Box
-          sx={{
-            display: 'flex',
-            Direction: 'row',
-            alignItems: 'space around',
-            marginBottom: 0,
-          }}
-        >
+    <Grid container spacing={{ xs: 12, md: 9, xl: 3 }} justifyContent="end">
+    {/* Image and Text Section */}
+    <Grid item xs={12} md={10}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row', 
+          alignItems: 'center',
+          marginBottom: 0,
+        }}
+      >
           {/* Image */}
           <img
             src={Graphic}
             alt="laptop open to Basil"
-            style={{ width: '100%', maxWidth: 400, height: 'auto' }}
+            style={{
+              width: '100%', // Make the image take 100% of the container
+              maxWidth: 400, // Limit the maximum width
+              height: 'auto',
+              objectFit: 'cover', // Prevent distortion
+            }}
           />
+
 
           {/* Text Box */}
           <Box
             sx={{
-              marginTop: 0,
-              textAlign: 'block',
+              textAlign: 'end',
             }}
           >
             <Box />
             <Grid />
-            <Grid container spacing={{ xs: 4, md: 3 }} justifyContent={'block'}>
+            <Grid container spacing={{ xs: 6 }} justifyContent={'block'}>
               <Grid item xs={6} md={10}>
                 <Box>
                   <Typography variant="h3"> 👋 Hey there! We are Basil. </Typography>
@@ -43,39 +48,34 @@ const Home = () => {
                 </Box>
               </Grid>
             </Grid>
-            </Box>
           </Box>
+        </Box>
       </Grid>
 
       {/* Paper Components Section */}
-      <Grid item xs={12}>
+      <Grid container spacing={{ xs: 2, md: 3, xl: 4 }} columns={{ xs:2, md: 7, xl: 12 }} justifyContent={'center'}>
         <Box
           sx={{
-            display: 'flex',
+            display: "flex"  ,
             flexWrap: 'wrap',
-        
             '& > :not(style)': {
+
               m: 4,
               p: 2,
               width: 260,
               height: 255,
+
             },
           }}
         >
-          <Paper elevation={3}>
-            "Absolutely love this budgeting app! It's user-friendly, visually
-            appealing, and has transformed my financial habits. The intuitive
-            features make budgeting a breeze, providing insightful analytics. A
-            must-have for anyone seeking financial control!" - Sophia R.
-          </Paper>
-          <Paper elevation={3}>"Impressive! This app simplifies budgeting like never before. Alex-friendly interface, powerful tools, and insightful reports. My financial game has never been stronger!" - Alex T.</Paper>
-          <Paper elevation={3}>"Five stars for this brilliant budgeting app! Its simplicity and effectiveness makes using it FUN! From tracking expenses to setting goals, it's the financial companion everyone needs. A total game-changer!" -Olivia H.</Paper>
-          <Paper elevation={3}>"Basil changed my life! This app is a game-changer. It effortlessly guides you through budgeting, offers clear insights, and motivates financial growth. A must for stress-free money management!" -Emily C.</Paper>
-          {/* Add more Paper components as needed */}
+          <Paper varient='elevation'>"Absolutely love this budgeting app! It's user-friendly, visually appealing, and has transformed my financial habits. The intuitive features make budgeting a breeze, providing insightful analytics. A must-have for anyone seeking financial control!" - Sophia R.</Paper>
+          <Paper varient='elevation'>"Impressive! This app simplifies budgeting like never before. Alex-friendly interface, powerful tools, and insightful reports. My financial game has never been stronger!" - Alex T.</Paper>
+          <Paper varient='elevation'>"Five stars for this brilliant budgeting app! Its simplicity and effectiveness makes using it FUN! From tracking expenses to setting goals, it's the financial companion everyone needs. A total game-changer!" -Olivia H.</Paper>
+          <Paper varient='elevation'>"Basil changed my life! This app is a game-changer. It effortlessly guides you through budgeting, offers clear insights, and motivates financial growth. A must for stress-free money management!" -Emily C.</Paper>
         </Box>
       </Grid>
-    </Grid>
-  );
+      </Grid>
+      );
 };
 
-export default Home;
+      export default Home;
