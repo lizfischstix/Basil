d0a56121b505f06c5220d35e413ab8e2784b56dc
Liz FIschenich <Liz.Devlin85@gmail.com>
2023-12-20 19:12:45 -0600

    redo header with mui

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,11 +1,33 @@
+import { Typography } from '@mui/material';
+import { Box, flexbox, fontFamily, typography } from '@mui/system';
 import React from 'react';
 import { Animated } from 'react-animated-css';
+import '@fontsource/lobster-two';
+import Banner from '../../assets/Images/BasilHeader.png';
+
 
 const Header = () => {
+  const CustomGrn = '#2a3a10';
   return (
-    <div className='header'>
-    <Animated animationIn='fadeInRightBig'><div className='headerText'> Basil </div></Animated>
-    </div>
+   
+      <Box
+        style={{
+          backgroundImage: `url(${Banner})`,
+          backgroundSize: 'cover', 
+          backgroundPosition: 'center', 
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Animated animationIn='fadeInRightBig'>
+          <Typography variant='h1' style={{ fontFamily: 'Lobster Two, sans-serif', color: CustomGrn }}>
+            Basil
+          </Typography>
+        </Animated>
+      </Box>
+   
   );
 };
 
